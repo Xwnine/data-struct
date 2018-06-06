fa55a6c61413adbb4e42a424fd5ba526445b8446
awen <huangwen@puyiwm.com.cn>
2018-06-06 14:33:42 +0800

    冒泡排序算法实现

--- bubble_sort.playground/Contents.swift
+++ bubble_sort.playground/Contents.swift
@@ -4,28 +4,20 @@ import UIKit
 
 var str = "冒泡排序"
 
+//冒泡排序最基本思想：两两比较相邻记录的关键字，如果相反则交换，知道没有反序为止
 
 
-/// 交换函数
+/// 基本的交换排序（不满足比较相邻记录）
 ///
-/// - Parameters:
-///   - array: 数据
-///   - i: 要交换的下标
-///   - j: 要交换的下标
-func swapInt(array: inout [Int], i: Int, j: Int) {
-	let temp = array[i]
-	array[i] = array[j]
-	array[j] = temp
-}
-
-
-func bubble_sort_0(array: inout [Int]) {
+/// - Parameter array: 数组
+func swap_sort(array: inout [Int]) {
 	var i = 0, j = 0
 	while i < array.count {
 		j = i+1
-		while j <= array.count {
-			if array[i] < array[j] {
-				swapInt(array: &array, i: i, j: j)
+		while j < array.count {
+			if array[i] > array[j] {
+				_swapInt(array: &array, i: i, j: j)
+				print("swap_sort: i=\(i) j=\(j)\n\(array)")
 			}
 			j += 1
 		}
@@ -34,14 +26,71 @@ func bubble_sort_0(array: inout [Int]) {
 }
 
 
-func bubble_sort_1(array: inout [Int]) {
+/// 冒泡排序
+///
+/// - Parameter array: [Int]
+func bubble_sort(array: inout [Int]) {
 	var i=0, j=0
-	while i<array.count {
-		j = array.count-1
+	while i < array.count {
+		j = array.count-1 //反向循环
 		while j >= i {
-			swapInt(array: &array, i: i, j: j)
+			if array[i] > array[j] {
+				_swapInt(array: &array, i: i, j: j)
+				print("bubble_sort i=\(i) j=\(j)\n\(array)")
+			}
 			j -= 1
 		}
 		i += 1
 	}
 }
+
+
+/// 冒泡排序优化后的算法，可以过滤掉对已有序的数据的y无意义循环判断
+/// 
+/// - Parameter array: [Int]
+func bubble_sort_pro(array: inout [Int]) {
+	
+	var flag = true /* flag 作为标记 */
+	var i = 0, j = 0
+	while i < array.count && flag {
+		flag = false
+		j = array.count - 1
+		while j >= i {
+			if array[i] > array[j] {
+				_swapInt(array: &array, i: i, j: j)
+				flag = true  /*有交换数据，flag 为true */
+				print("bubble_sort_pro i=\(i) j=\(j)\n\(array)")
+			}
+			j -= 1
+		}
+		i += 1
+	}
+}
+
+
+/// 交换函数
+///
+/// - Parameters:
+///   - array: 数据
+///   - i: 要交换的下标
+///   - j: 要交换的下标
+func _swapInt(array: inout [Int], i: Int, j: Int) {
+	let temp = array[i]
+	array[i] = array[j]
+	array[j] = temp
+}
+
+
+//MARK： test
+
+var arrayInt = [9,1,5,8,3,7,6,4,2];
+swap_sort(array: &arrayInt)
+
+print("\n-------------------------------\n")
+
+arrayInt = [9,1,5,8,3,7,6,4,2];
+bubble_sort(array: &arrayInt)
+print("\n-------------------------------\n")
+
+arrayInt = [2,1,3,4,6,5,7,8,9]
+bubble_sort_pro(array: &arrayInt)
